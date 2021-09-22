e19f28ed8a054d9f1764d44bae46fddc08706aa7
Naman Vaishnav <jhanvi.vaishnav00@gmail.com>
2021-09-22 15:02:39 +0530

    Changes in Chip module

--- lib/screens/assignment9.dart
+++ lib/screens/assignment9.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:ildc_hw/screens/second_page.dart';
 
 class Assignment9 extends StatefulWidget {
@@ -10,27 +12,57 @@ class Assignment9 extends StatefulWidget {
 }
 
 class _Assignment9State extends State<Assignment9> {
+  List<String> data = [
+    "Mehul Patel",
+    "Nikunj",
+    "Bhargav",
+    "Devang",
+    "Harsh",
+    "Ami"
+  ];
+
+  Iterable<Widget> get chipWidgets sync* {
+    for (final String chipText in data) {
+      yield Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: isRemove
+            ? Chip(
+                label: Text(chipText),
+                onDeleted: () {
+                  setState(() {
+                    (data.length == 10) ? ishide = !ishide : ishide;
+                    data.removeWhere((String entry) {
+                      return entry == chipText;
+                    });
+                  });
+                },
+              )
+            : Chip(
+                label: Text(chipText),
+              ),
+      );
+    }
+  }
+
   var controller = TextEditingController();
   bool isRemove = false;
-  List data = [
-    "Flutter",
-    "Dart",
-    "Java",
-  ];
+  bool ishide = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-          child: const Icon(
-            Icons.navigate_next_rounded,
-            size: 40,
+        child: const Icon(
+          Icons.navigate_next_rounded,
+          size: 40,
+        ),
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SecondScreen(),
           ),
-          onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const SecondScreen(),
-              ))),
+        ),
+      ),
       body: SafeArea(
         child: Center(
           child: Padding(
@@ -38,54 +70,69 @@ class _Assignment9State extends State<Assignment9> {
             child: Column(
               children: [
                 TextFormField(
+                  onTap: () {
+                    FocusScopeNode currentFocus = FocusScope.of(context);
+                    if (!currentFocus.hasPrimaryFocus) {
+                      currentFocus.unfocus();
+                    }
+                  },
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z\\s]")),
+                  ],
                   controller: controller,
                   decoration: InputDecoration(
-                    prefixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            if (controller.text.isNotEmpty) {
-                              data.add(controller.text);
-                              print(data);
-                              print(controller.text);
-                            } else {
-                              showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return CupertinoAlertDialog(
-                                    title: const Text('Please Enter your Text'),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () async {
-                                          Navigator.pop(context);
-                                        },
-                                        child: const Text('OK'),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            }
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.add,
-                          color: Colors.blue,
-                        )),
-                    suffixIcon: TextButton(
-                      onPressed: () {
-                        setState(() {
-                          // data.remove(controller.text);
-                          isRemove = !isRemove;
-                          print(data);
-                        });
-                      },
-                      child: const Text("Edit"),
-                    ),
+                    prefixIcon: ishide
+                        ? IconButton(
+                            onPressed: () {
+                              if (data.contains(controller.text)) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return CupertinoAlertDialog(
+                                      title: const Text('Data Already Exist'),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () async {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text('OK'),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              } else {
+                                onPressed(context);
+                              }
+
+                              controller.clear();
+                            },
+                            icon: const Icon(
+                              Icons.add,
+                              color: Colors.blue,
+                            ),
+                          )
+                        : Container(
+                            width: 4,
+                          ),
+                    suffixIcon: (data.length > 0)
+                        ? TextButton(
+                            onPressed: () {
+                              setState(() {
+                                // data.remove(controller.text);
+                                
+                                isRemove = !isRemove;
+                                print(data);
+                              });
+                            },
+                            child: const Text("Edit"),
+                          )
+                        : Container(
+                            width: 10,
+                          ),
                   ),
                 ),
-                Wrap(
-                  children: chips(),
-                )
+                Wrap(children: chipWidgets.toList())
               ],
             ),
           ),
@@ -94,33 +141,86 @@ class _Assignment9State extends State<Assignment9> {
     );
   }
 
-  List<Widget> chips() {
-    List<Widget> chips = [];
-    for (int i = 0; i < data.length; i++) {
-      Widget item = Padding(
-        padding: const EdgeInsets.only(left: 10, right: 5),
-        child: isRemove
-            ? InputChip(
-                disabledColor: Colors.grey[300],
-                label: Text(data[i]),
-                isEnabled: true,
-                onDeleted: () {
-                  data.removeAt(i);
-                  setState(
-                    () {
-                      data = data;
-                    },
-                  );
-                },
-              )
-            : InputChip(
-                disabledColor: Colors.grey[300],
-                label: Text(data[i]),
-                isEnabled: true,
-              ),
-      );
-      chips.add(item);
-    }
-    return chips;
+  void onPressed(BuildContext context) {
+    return setState(() {
+      print(ishide);
+      (data.length == 10) ? ishide = !ishide : ishide;
+      print(ishide);
+      print(controller.text.trim());
+      if (data.length < 10 &&
+          controller.text.trim().isNotEmpty &&
+          (controller.text != " ")) {
+        data.add(controller.text.trim());
+
+        print(data);
+        print(controller.text);
+      } else if (data.length == 10 && controller.text.trim().isNotEmpty) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return CupertinoAlertDialog(
+              title: const Text('you reached at range'),
+              actions: [
+                TextButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return CupertinoAlertDialog(
+              title: const Text('Enter your text'),
+              actions: [
+                TextButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    });
   }
 }
+//   List<Widget> chips() {
+//     List<Widget> chips = [];
+//     for (int i = 0; i < data.length; i++) {
+//       Widget item = Padding(
+//         padding: const EdgeInsets.only(left: 10, right: 5),
+//         child: isRemove
+//             ? InputChip(
+//                 disabledColor: Colors.grey[300],
+//                 label: Text(data[i]),
+//                 isEnabled: true,
+//                 onDeleted: () {
+//                   data.removeAt(i);
+//                   setState(
+//                     () {
+//                       data = data;
+//                     },
+//                   );
+//                 },
+//               )
+//             : InputChip(
+//                 disabledColor: Colors.grey[300],
+//                 label: Text(data[i]),
+//                 isEnabled: true,
+//               ),
+//       );
+//       chips.add(item);
+
+//       controller.clear();
+//     }
+//     return chips;
+//   }
+// }
